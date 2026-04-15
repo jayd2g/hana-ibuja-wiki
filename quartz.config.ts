24c5c54725f8ace8556740245fdc9d9ff9677d26
jayd2g <genojd@gmail.com>
2026-04-15 11:35:04 +0900

    fix baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jayd2g.github.io/hana-ibuja-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
