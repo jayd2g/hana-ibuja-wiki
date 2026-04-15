33188bf934701424cee88bfa2fb91c61e601c329
jayd2g <genojd@gmail.com>
2026-04-15 11:48:29 +0900

    add recent notes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [Component.RecentNotes({ title: "최근 업데이트", limit: 10 })],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
